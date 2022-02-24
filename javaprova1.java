f4ce8e2590c9d087da1f7d6a0d49f9c81eaaa382
Ismael <ismaelsampol@gmail.com>
2022-02-24 16:23:01 +0100

    he añadido otro hola

--- javaprova1.java
+++ javaprova1.java
@@ -2,5 +2,6 @@ public class javaprova1{
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Hola2");
+        System.out.println("Hola3");
     }
 }
